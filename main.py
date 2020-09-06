643cf00fea34c791837e6a1e4f045c3a6160e4cc
khizar-k <khiz_k@outlook.com>
2020-09-05 22:48:47 -0700

    second

--- main.py
+++ main.py
@@ -32,38 +32,38 @@ except:
         if test['tag'] not in labels:
             labels.append(test['tag'])
 
-    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-    words = sorted(list(set(words)))
+  words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+  words = sorted(list(set(words)))
 
-    labels = sorted(labels)
+  labels = sorted(labels)
 
-    training = []
-    output = []
+  training = []
+  output = []
 
-    out_empty = [0 for _ in range(len(labels))]
+  out_empty = [0 for _ in range(len(labels))]
 
-    for x, doc in enumerate(docs_x):
-        bag = []
+  for x, doc in enumerate(docs_x):
+      bag = []
 
-        wrds = [stemmer.stem(w.lower()) for w in doc]
+      wrds = [stemmer.stem(w.lower()) for w in doc]
 
-        for w in words:
-            if w in wrds:
-                bag.append(1)
-            else:
-                bag.append(0)
+      for w in words:
+          if w in wrds:
+              bag.append(1)
+          else:
+              bag.append(0)
 
-        output_row = out_empty[:]
-        output_row[labels.index(docs_y[x])] = 1
+      output_row = out_empty[:]
+      output_row[labels.index(docs_y[x])] = 1
 
-        training.append(bag)
-        output.append(output_row)
+      training.append(bag)
+      output.append(output_row)
 
-    training = numpy.array(training)
-    output = numpy.array(output)
+  training = numpy.array(training)
+  output = numpy.array(output)
 
-    with open("data.pickle", "wb") as f:
-        pickle.dump((words, labels, training, output), f)
+  with open("data.pickle", "wb") as f:
+      pickle.dump((words, labels, training, output), f)
 
 tensorflow.reset_default_graph()
 
@@ -81,18 +81,18 @@ except:
     model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
     model.save("model.tflearn")
 
-    def bag_of_words(s, words):
-    bag = [0 for _ in range(len(words))]
+def bag_of_words(s, words):
+bag = [0 for _ in range(len(words))]
 
-    s_words = nltk.word_tokenize(s)
-    s_words = [stemmer.stem(word.lower()) for word in s_words]
+s_words = nltk.word_tokenize(s)
+s_words = [stemmer.stem(word.lower()) for word in s_words]
 
-    for se in s_words:
-        for i, w in enumerate(words):
-            if w == se:
-                bag[i] = 1
+for se in s_words:
+    for i, w in enumerate(words):
+        if w == se:
+            bag[i] = 1
 
-    return numpy.array(bag)
+return numpy.array(bag)
 
 ###########################
 
